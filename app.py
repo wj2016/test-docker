307ea3a2cc9484904e35bd9200421c1737f9420e
Jia Wang <jia@doorling.se>
2019-01-15 18:11:39 +0100

    different hash

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker, Jenkins, '
+    return 'Hello, Docker, Jenkins, AWS, gogogo'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
